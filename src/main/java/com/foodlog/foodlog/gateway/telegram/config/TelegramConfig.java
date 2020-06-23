1c0d04ab075afb4d1ee7cd03e6821ecd846ab7bd
rafael <rafaelhss@gmail.com>
2020-06-23 16:51:34 -0300

    rezar

--- src/main/java/com/foodlog/foodlog/gateway/telegram/config/TelegramConfig.java
+++ src/main/java/com/foodlog/foodlog/gateway/telegram/config/TelegramConfig.java
@@ -6,6 +6,6 @@ package com.foodlog.foodlog.gateway.telegram.config;
 public class TelegramConfig {
     public static final String BOT_ID_VELHO = "380968235:AAGqnrSERR8ABcw-_avcPN2ES3KH5SeZtNM";
     public static final String BOT_ID_FOODLOG2 = "496888504:AAE3mU83hGmKfSYn0Z8vEhA-tDYM4lXOdI4";
-    public static final String BOT_ID = "664657420:AAHSq3y0Z3WN1UZHt2MhfQvf0kQbxQCwYBU";
-
+    public static final String BOT_IDvelho = "664657420:AAHSq3y0Z3WN1UZHt2MhfQvf0kQbxQCwYBU";
+    public static final String BOT_ID = "1197511596:AAGeb1HFGUUMPuDjn_vNFx9CeOZ83gNluPo";
 }
